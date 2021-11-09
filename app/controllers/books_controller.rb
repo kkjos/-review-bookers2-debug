b798ce2ceddd52b546108bc8f0d85c84979cec75
kkjos <k19951227@gmail.com>
2021-11-09 04:12:02 +0000

    [finish]応用課題9d

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -53,7 +53,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :rate)
+    params.require(:book).permit(:title, :body, :category, :rate)
   end
 
 end
